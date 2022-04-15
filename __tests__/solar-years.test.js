a61cbbb0c72dc96b2bbd80540981c02a9798b6e9
Mark McConnell <amark.mcconnell@gmail.com>
2022-04-15 10:23:57 -0700

    test 4 written

--- __tests__/solar-years.test.js
+++ __tests__/solar-years.test.js
@@ -1,21 +1,29 @@
 import Human from '../src/js/solar-years.js';
 
 describe('Human', () =>  {
+let human;
+
+beforeEach(() => {
+  human = new Human ("Mark", 35);
+})
+
   test('should correctly create a human object', () => {
-    const human = new Human ("Mark", 35);
     expect(human.name).toEqual("Mark");
     expect(human.age).toEqual(35); 
   });
 
   test('should use this.age to find Mercury years to correct value', () => {
-    const human = new Human ("Mark", 35);
     let mercuryYears = human.mercury();
     expect(mercuryYears).toEqual(145);
   });
 
   test('should use this.age to find Venus years', () => {
-    const human = new Human ("Mark", 35);
     let venusYears = human.venus();
     expect(venusYears).toEqual(56);
+  })
+
+  test('should use this.get to find Mars years', () => {
+    let marsYears = human.mars();
+    expect(marsYears).toEqual()
   })
 });
