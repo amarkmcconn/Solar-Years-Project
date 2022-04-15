16d6119f4b93fdb24462a16edd83218bce8cdf4b
Mark McConnell <amark.mcconnell@gmail.com>
2022-04-15 09:07:01 -0700

    test 1 update for red workflow.

--- __tests__/solar-years.test.js
+++ __tests__/solar-years.test.js
@@ -2,4 +2,7 @@ import Human from '../src/js/solar-years.js';
 
 describe('Human', () =>  {
   test('should correctly create a human object')
+  const human = new Human ("Mark", 35);
+  expect(human.name).toEqual("Mark");
+  expect(human.name).toEqual(35);
 })
